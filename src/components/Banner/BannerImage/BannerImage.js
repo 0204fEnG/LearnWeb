fa1a9767c44a10a76eaad7ae771e92f0a89e4442
feng <794761652@qq.com>
2025-01-28 20:31:53 +0800

    1/28

--- src/components/Banner/BannerImage/BannerImage.js
+++ src/components/Banner/BannerImage/BannerImage.js
@@ -1,10 +1,60 @@
 import './BannerImage.scss'
-const BannerImage = ({ targetImage=0,images}) => {
+import { useEffect, useRef, useState } from 'react'
+const BannerImage = ({ images }) => {
+  const [imageIndex, setImageIndex] = useState(1)
+  const length=images.length
+  // const [indexScroll,setIndexScroll]=useState()
+  const imgDiv = useRef()
   const allImages = [images[images.length - 1], ...images, images[0]]
+  const handleClickScroll = (index) => {
+    const currentScrollWidth = imgDiv.current.offsetWidth
+    imgDiv.current.scrollTo({
+      left: currentScrollWidth * index,
+      behavior:'smooth'
+    })
+  }
+  useEffect(() => {
+    const initScroll = () => {
+      const currentScrollWidth = imgDiv.current.offsetWidth
+          imgDiv.current.scrollTo({
+            left: currentScrollWidth * imageIndex,
+            behavior:'instant'
+    })
+    }
+    const handleImagesScrollEnd = () => {
+      const currentScrollWidth = imgDiv.current.offsetWidth
+      const currentScrollLeft = imgDiv.current.scrollLeft
+      const currentIndex = Math.round(currentScrollLeft / currentScrollWidth)
+      if (currentIndex === 0) {
+        imgDiv.current.scrollTo({
+          left: currentScrollWidth * length,
+            behavior: 'instant'
+        })
+      }
+      else if (currentIndex === length + 1) {
+        imgDiv.current.scrollTo({
+          left: currentScrollWidth,
+          behavior:'instant'
+        })
+      }
+      else {
+        setImageIndex(currentIndex)
+      }
+    }
+    initScroll()
+    imgDiv.current.addEventListener('scrollsnapchange', handleImagesScrollEnd)
+    window.addEventListener('resize',initScroll)
+  },[])
   return (
-    <div className="banner-image-container" style={{ backgroundImage: `url(${images[targetImage]})` }} >
-      <div className="blur"></div>
-      { allImages.map((image, index) => <img src={image} key={index} className='banner-image' alt='轮播图'></img>)}
+    <div className="banner-image-container" style={{ backgroundImage: `url(${allImages[imageIndex]})` }} >
+      <div className="blur" ref={imgDiv}>
+        {allImages.map((image, index) => <img src={image} key={index} className='banner-image' alt='轮播图'></img>)}
+      </div>
+      <div className="changeIndex prev" onClick={()=>handleClickScroll(imageIndex-1)}>&lt;</div>
+      <div className="changeIndex next" onClick={() =>handleClickScroll(imageIndex+1)}>&gt;</div>
+      <div className="imageIndexs">
+        {images.map((_,index) => <div className={['imageIndex', index + 1 === imageIndex? 'current' : ''].join(' ')} key={index} onClick={()=>handleClickScroll(index+1)}></div>)}
+      </div>
     </div>
   )
 }
--- src/pages/Home/HomeRecommend/HomeRecommend.js
+++ src/pages/Home/HomeRecommend/HomeRecommend.js
@@ -1,18 +1,16 @@
+import { useState } from 'react'
 import Banner from '../../../components/Banner/Banner'
 import './HomeRecommend.scss'
 // import PostCard from '../../../components/ContentCard/PostCard/PostCard.js'
 const HomeRecommend = () => {
     const videoUrl = '/videos/ban.mp4'
     const imgUrl = [
-"/images/header/banner/小小陈.png",
-"/images/header/banner/unlock_wallpaper_1.jpg",
-"/images/header/banner/unlock_wallpaper_2.jpg",
-"/images/header/banner/unlock_wallpaper_3.jpg",
-"/images/header/banner/unlock_wallpaper_4.jpg",
-"/images/header/banner/unlock_wallpaper_5.jpg",
-"/images/header/banner/unlock_wallpaper_6.jpg",
-"/images/header/banner/unlock_wallpaper_7.jpg",
-"/images/header/banner/unlock_wallpaper_8.jpg",
+"/images/header/banner/春霖翠意.jpg",
+"/images/header/banner/迭翠千里.jpg",
+"/images/header/banner/冬夜小镇.jpg",
+"/images/header/banner/星蕴蓝.jpg",
+"/images/header/banner/诗野千里.jpg",
+"/images/header/banner/星蕴金.jpg",
     ]
     return (
         <div className="home-recommend">
